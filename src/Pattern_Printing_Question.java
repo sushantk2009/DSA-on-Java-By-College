42e3b52ef490971875ffb2eb1349d2e3125ba4c7
Sushant Kushwaha <shushantk2009@gmail.com>
2024-11-24 00:04:48 +0530

    Pattern questions

--- src/Pattern_Printing_Question.java
+++ src/Pattern_Printing_Question.java
@@ -10,12 +10,128 @@ public class Pattern_Printing_Question {
 //        }
 
         //PROBLEM :2
-        for(int i=1;i<=4;i++){
-            for(int j=4;j>=i;j--){
+//        for(int i=1;i<=4;i++){
+//            for(int j=4;j>=i;j--){
+//                System.out.print("*");
+//            }
+//            System.out.println(" ");
+//        }
+
+
+        //PROBLEM :3
+//        for(int i=1;i<=4;i++){
+//            for(int j=1;j<=i;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//        for(int i=1;i<=3;i++){
+//            for (int j=3;j>=i;j--){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+        //PROBLEM :4
+//        for(int i=1;i<=5;i++){
+//            for (int j=4;j>=i;j--){
+//                System.out.print(" ");
+//            }
+//                for(int l=1;l<=i;l++){
+//                    System.out.print("*");
+//                }
+//                System.out.println();
+//            }
+
+
+        //PROBLEM 5:
+//        for (int i=1;i<=4;i++){
+//            for (int j=4;j>=i;j--){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//            for(int k=1;k<=i;k++){
+//                System.out.print(" ");
+//            }
+//        }
+
+
+        //PROBLEM 6:
+//        for(int i=1;i<=4;i++){
+//            for (int j=3;j>=i;j--){
+//                System.out.print(" ");
+//            }
+//            for(int k=1;k<=i;k++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//        for(int i=1;i<=3;i++){
+//            for (int j=1;j<=i;j++){
+//                System.out.print(" ");
+//            }
+//            for (int k=3;k>=i;k--){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+
+        //PROBLEM 7:
+//        for(int i=1;i<=4;i++){
+//            for (int j=3;j>=i;j--){
+//                System.out.print(" ");
+//            }
+//            for (int k=1;k<=i;k++){
+//                System.out.print(" *");
+//            }
+//            System.out.println();
+//        }
+
+        //PROBLRM 8:
+//        for (int i=1;i<=4;i++){
+//            for (int j=3;j>=i;j--){
+//                System.out.print(" ");
+//            }
+//            for (int k=2;k<=i;k++){
+//                System.out.print("*");
+//            }
+//            for (int l=1;l<=i;l++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+
+        //PROBLEM 9:
+        for (int i=1;i<=4;i++){
+            for (int j=2;j<=i;j++){
+                System.out.print(" ");
+            }
+            for (int k=4;k>=i;k--){
                 System.out.print("*");
             }
-            System.out.println(" ");
+            for(int l=3;l>=i;l--){
+                System.out.print("*");
+            }
+            System.out.println();
         }
 
+
+        //PROBLEM 10:
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
