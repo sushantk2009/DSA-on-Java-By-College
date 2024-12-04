f790d1797e0216738a2117c8d771ccb858b64719
Sushant Kushwaha <146377328+sushantk2009@users.noreply.github.com>
2024-12-04 22:44:38 +0530

    Update Pattern_Printing_Question.java

--- src/Pattern_Printing_Question.java
+++ src/Pattern_Printing_Question.java
@@ -4,7 +4,7 @@ public class Pattern_Printing_Question {
     public static void main(String[]args) {
 
         //PROBLEM :1
-        System.out.println("PROBLEM 1:");
+        System.out.println("PROBLEM  1:");
         for (int i = 1; i <= 4; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
