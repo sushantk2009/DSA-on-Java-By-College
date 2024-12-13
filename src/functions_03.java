46758be58dfa153be42fd52f9307b6a52a8c42e5
Sushant Kushwaha <shushantk2009@gmail.com>
2024-12-13 22:33:19 +0530

    Hacker rank

--- src/functions_03.java
+++ src/functions_03.java
@@ -1,5 +1,19 @@
-public class functions_03 {
+//Pass by value
 
+public class functions_03 {
+    public static void fun(int a, int b) {
+     a=3;
+     b=4;
+    }
+public static void main(String[]args){
+    int x=1;
+    int y=2;
+    System.out.println(x+" "+y);
+    fun(x,y);
+    System.out.println(x+" "+y);
+
+
+}
 
 
 }
---
+++ src/hacker_rank_1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class hacker_rank_1 {
+    public static void main(String[] args) {
+        //PROBLEM 1:
+        int x,y,z;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter X : ");
+        x=sc.nextInt();
+        System.out.println("Enter Y : ");
+        y=sc.nextInt();
+        System.out.println("Enter Z : ");
+        z=sc.nextInt();
+        if (x>=0 && x<=100 && y>=0 && y<=100 && z>=0 && z<=100){
+            if (x>y){
+                if(x>z)
+                    System.out.println("The Largest Number is : "+x);
+                else
+                    System.out.println("The Largest Number is : "+z);
+            }
+            else {
+                if (y>z)
+                    System.out.println("The Largest Number is : "+y);
+                else
+                    System.out.println("The Largest Number is : "+z);
+            }
+        }
+    }
+}
