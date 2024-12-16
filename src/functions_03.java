74eeb55180b56b9033ceabe949f40436c31bd7f6
Sushant Kushwaha <146377328+sushantk2009@users.noreply.github.com>
2024-12-16 19:29:19 +0530

    Update functions_03.java

--- src/functions_03.java
+++ src/functions_03.java
@@ -8,7 +8,7 @@ public class functions_03 {
 public static void main(String[]args){
     int x=1;
     int y=2;
-    System.out.println(x+" "+y);
+    System.out.println(x+"  "+y);
     fun(x,y);
     System.out.println(x+" "+y);
 
